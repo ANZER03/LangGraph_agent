ba4564ca62906ae4d642c237ee556bf3908b8f44
Anouar Zerrik <an_zerrik@etu.enset-media.ac.ma>
2025-11-12 13:13:34 +0100

    delete the api key

--- llm.py
+++ llm.py
@@ -1,7 +1,7 @@
 from langchain.chat_models import init_chat_model
 import os
 
-llm = init_chat_model("google_genai:gemini-2.5-flash-lite" , api_key="AIzaSyDibK3sxWVNE3BN8zeKvuVLxXI1-ONkKiE")
+llm = init_chat_model("google_genai:gemini-2.5-flash-lite" , api_key="your-api-key")
 
 ASSISTANT_SYSTEM_PROMPT = (
     "You are TaskMate, a proactive task management assistant.\n"
